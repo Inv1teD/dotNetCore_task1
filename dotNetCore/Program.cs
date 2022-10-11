cc59fb584f9494fa12258c8a3bbd4e92b489c6f6
Inv1teD <prudnikovdaniil485@gmail.com>
2022-10-11 19:35:23 +0200

    REST API with MapControllers

--- dotNetCore/Program.cs
+++ dotNetCore/Program.cs
@@ -1,9 +1,12 @@
 using dotNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace dotNetCore
 {
@@ -27,145 +30,40 @@ namespace dotNetCore
                 .UseNpgsql(connectionString)
                 .Options;
 
+            var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+            builder.Services.AddRazorPages();
 
-            using (ApplicationContext db = new ApplicationContext(options))
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
             {
-                var builder = WebApplication.CreateBuilder(args);
+                app.UseExceptionHandler("/Error");
 
-                // Add services to the container.
-                builder.Services.AddRazorPages();
-
-                var app = builder.Build();
-
-                // Configure the HTTP request pipeline.
-                if (!app.Environment.IsDevelopment())
-                {
-                    app.UseExceptionHandler("/Error");
-                    
-                    app.UseHsts();
-                }
-
-
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.UseAuthorization();
-
-                app.MapRazorPages();
-
-                app.Run(async (context) =>
-                {
-                    var response = context.Response;
-                    var request = context.Request;
-                    var path = request.Path;
-                    string expressionForNumber = "^/api/users/[0-9]+$";  
-
-
-                    Console.WriteLine(path);
-
-                    if (path == "/api/users" && request.Method == "GET")
-                    {
-                        await GetAllPeople(response, db);
-                    }
-                    else if ((Regex.IsMatch(path, expressionForNumber)) && request.Method == "GET")
-                    {
-                        int? id = int.Parse(path.Value?.Split("/")[3]);
-                        await GetPerson(response, id);
-
-                    }
-                    else if (path == "/api/users" && request.Method == "POST")
-                    {
-                        await CreatePerson(response, request);
-                    }
-                    else if ((Regex.IsMatch(path, expressionForNumber)) && request.Method == "DELETE")
-                    {
-                        int? id = int.Parse(path.Value?.Split("/")[3]);
-                        await DeletePerson(response, id);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Loaded base page");
-                        response.ContentType = "text/html; charset=utf-8";
-                        await response.SendFileAsync("Pages/Index.cshtml");
-                    }
-                });
-
-
-                app.Run();
-
-                // Gets all users
-                static async Task GetAllPeople(HttpResponse response, ApplicationContext db)
-                {
-                    var ukrainians = db.Ukrainians.ToList();
-                    await response.WriteAsJsonAsync(ukrainians);
-                }
-                // Gets one user with specific id
-                async Task GetPerson(HttpResponse response, int? id)
-                {
-
-                    Ukrainian? ukrainian = db.Ukrainians.FirstOrDefault((u) => u.Id == id);
-                    // If user exist, sending him
-
-                    if (ukrainian != null)
-                        await response.WriteAsJsonAsync(ukrainian);
-                    // If user doesn't exist, sending status code and error message
-                    else
-                    {
-                        response.StatusCode = 404;
-                        await response.WriteAsJsonAsync(new { message = "User wasn't found" });
-                    }
-                }
-                    
-                async Task CreatePerson(HttpResponse response, HttpRequest request)
-                    {
-                    
-                        try
-                        {
-                        var ukrainian = await request.ReadFromJsonAsync<Ukrainian>();
-
-                        if (ukrainian != null)
-                            {
-                                db.Ukrainians.Add(ukrainian);
-                                db.SaveChanges();
-                                await response.WriteAsJsonAsync(ukrainian);
-                            }
-                            else
-                            {
-                                throw new Exception("Incorrect data");
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            response.StatusCode = 400;
-                            await response.WriteAsJsonAsync(new { message = "Incorrect data" });
-                        }
-                    }
-                async Task DeletePerson(HttpResponse response, int? id)
-                {
-
-                    Ukrainian? ukrainian = db.Ukrainians.FirstOrDefault((u) => u.Id == id);
-
-                  
-                    if (ukrainian != null)
-                    {
-                        db.Ukrainians.Remove(ukrainian);
-                        db.SaveChanges();
-                        await response.WriteAsJsonAsync(ukrainian);
-                    }
-                    else
-                    {
-                        response.StatusCode = 404;
-                        await response.WriteAsJsonAsync(new { message = "Incorrect param id" });
-                    }
-                    
-                }
-                }
+                app.UseHsts();
             }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();    
+            });
+            app.Run();
+
         }
     }
+}
+
 
 
 
